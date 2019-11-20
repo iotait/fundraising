5e8ab698a9000be2da59dc26a0bcc09aae8dcdca
Tanner Wernecke <tannerwernecke@iotait.com>
2019-11-20 10:44:36 -0500

    Update specs.
    After abstracting teacher, student and admin, the specs had to be
    updated.

--- spec/features/user_creates_admin_spec.rb
+++ spec/features/user_creates_admin_spec.rb
@@ -5,10 +5,11 @@ feature "User creates admin" do
     visit "/admins"
 
     click_on "Add a new admin"
-    fill_in "Name", with: "XYZ"
+    fill_in "admin_first_name", with: "Bill"
+    fill_in "admin_last_name", with: "Bob"
     fill_in "Email", with: "XYZ@gmail.com"
     fill_in "Phone", with: "0123456789"
     click_on "Submit"
-    expect(page).to have_css ".admins li", text: "XYZ"
+    expect(page).to have_css ".admins li", text: "Bill"
   end
 end
--- spec/features/user_creates_donation_spec.rb
+++ spec/features/user_creates_donation_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 feature "User creates donation" do
   scenario "successfully" do
-    @admin = Admin.create!(name: "Bill", email: "bill@incharge.com", phone: "3456789012")
+    @admin = Admin.create!(first_name: "Bill", last_name: "in charge", email: "bill@incharge.com", phone: "3456789012")
     @school = School.create!(admin_id: 1, name: "xyz", email: "xyz@gmail.com", phone: "1234567890", address: "123 school lane", about: "Buy desks", goal: 5000)
-    @teacher = Teacher.create!(school_id: 1, name: "Jeff", email: "j@gmail.com", phone: "0123456789", goal: 100)
-    @student = Student.create!(id: 1, school_id: 1, teacher_id: 1, name: "Bill", goal: 100)
+    @teacher = Teacher.create!(school_id: 1, first_name: "Jeff", last_name: "Jeffery", email: "j@gmail.com", phone: "0123456789", goal: 100)
+    @student = Student.create!(teacher_id: 2, first_name: "Bill", last_name: "Bunny", email: "bb@yahoo", about: "let's do this!", goal: 100)
     visit "/donations"
 
     click_on "Add a new donation"
-    fill_in "Student", with: 1
+    fill_in "Student", with: 3 #It is the third user created
     fill_in "Donor", with: "Grandpa"
     fill_in "Amount", with: 50
+    fill_in "Message", with: "Glad to help son!"
     click_on "Submit"
     expect(page).to have_css ".donations li", text: "Grandpa"
   end
