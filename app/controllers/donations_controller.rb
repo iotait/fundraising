8af6a0d28ab7a9ddeeb832975d43c6ce27d6e038
Tanner Wernecke <tannerwernecke@iotait.com>
2019-11-25 09:10:11 -0500

    Add missing actions.
    Several controllers were missing actions and we want to add that
    functionality to use in our app.
    
    Update controllers, views and routes.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,20 +1,31 @@
 class DonationsController < ApplicationController
+  before_action :set_donation, only: [:show]
+
+  # GET /donations
   def index
     @donations = Donation.all
-    render :index
   end
 
+  # GET /donations/1
+  def show
+  end
+
+  # GET /donations/new
   def new
     @donation = Donation.new
-    render :new
   end
 
+  # POST /donations
   def create
     Donation.create(donation_params)
     redirect_to donations_path
   end
 
   private
+
+  def set_donation
+    @donation = Donation.find(params[:id])
+  end
 
   def donation_params
     params.require(:donation).permit(:student_id, :amount, :donor, :message)
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,22 +1,43 @@
 class TeachersController < ApplicationController
-  before_action :set_teacher, only: [:show]
+  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
   before_action :set_students, only: [:show]
 
+  # GET /teachers
   def index
     @teachers = Teacher.all
-    render :index
   end
 
+  # GET /teachers/1
   def show
   end
 
+  # GET /teachers/new
   def new
     @teacher = Teacher.new
-    render :new
   end
 
+  # GET /teachers/1/edit
+  def edit
+  end
+
+  # POST /teachers
   def create
     Teacher.create(teacher_params)
+    redirect_to teachers_path
+  end
+
+   # PATCH/PUT /teachers/1
+   def update
+    if @teacher.update(teachers_path)
+      redirect_to teachers_path
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /teachers/1
+  def destroy
+    @teacher.destroy
     redirect_to teachers_path
   end
 
