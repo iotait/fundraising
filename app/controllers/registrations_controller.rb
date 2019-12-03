7ad8db8ce4d9b86a3cd270d5410a9885bf6b0b76
Tanner Wernecke <tannerwernecke@iotait.com>
2019-12-02 22:29:49 -0500

    Move registration logic out of view, into registrations controller.
    Logic was present in registrations/new.html.erb that shouldn't have been
    in the view. The logic to set the user registering as an admin was moved
    and placed into the registrations_controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+  
+  def create
+    super do
+      resource.type = "Admin"
+      resource.save
+    end
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
