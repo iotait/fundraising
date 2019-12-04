c64697fadc34b757278bbc7cdbd59ccb5f8f5357
Tanner Wernecke <tannerwernecke@iotait.com>
2019-12-04 13:32:17 -0500

    Refactor factories.
    Instead of having duplicate code in the user, student and admin
    factories, we refactor it to a single factory.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,18 @@ FactoryBot.define do
     sequence(:last_name) { |n| n.to_s }
     sequence(:email) { |n| "user#{n}@test.com" }
     password { "password" }
+
+    factory :student, class: Student, parent: :user do
+      association :school
+
+      factory :student_with_donation do
+        after(:create) do |student|
+          create(:donation, student: student)
+        end
+      end
+    end
+
+    factory :admin, class: Admin, parent: :user do
+    end
   end
 end
