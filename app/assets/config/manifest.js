0aca12c6122c8f02856b396e513d7138f8b9fdb2
Tanner Wernecke <tannerwernecke@iotait.com>
2019-12-19 12:04:35 -0500

    Overhaul update with Matt.
    * Massive overhaul update after working with Matt.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+<<<<<<< HEAD
 //= link ../javascripts/application.js
+=======
+//= link_directory ../javascripts .js
+>>>>>>> Overhaul update with Matt.
