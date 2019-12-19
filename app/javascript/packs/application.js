5b5205a7f5cac14a89b7243203f45ccbacc9379e
Tanner Wernecke <tannerwernecke@iotait.com>
2019-12-19 13:18:43 -0500

    Troubleshoot heroku deployment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,6 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("trix")
-require("@rails/actiontext")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
