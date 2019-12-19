38be7488974f774ca81622c06ab00e4321ae0d64
Tanner Wernecke <tannerwernecke@iotait.com>
2019-12-19 12:51:12 -0500

    Fix linking errors.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-<<<<<<< HEAD
-//= link ../javascripts/application.js
-=======
 //= link_directory ../javascripts .js
->>>>>>> Overhaul update with Matt.
