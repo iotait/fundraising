9e04f880e394c7537251dd81d7e4c3efbe020211
Tanner Wernecke <57360915+tannerwernecke@users.noreply.github.com>
2019-12-31 12:22:26 -0500

    Add action text to app. (#50)
    * When entering/editing a school's about page, the admin will want to work
    * with action text rather than a normal text space. We want to add
    * functionality for this.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_193603) do
+ActiveRecord::Schema.define(version: 2019_12_31_164143) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
