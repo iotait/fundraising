e864b512df692f9c2881ad4df3ac68f0b54fbf25
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2021-12-01 09:36:30 -0500

    Add hot reloading to webpack-dev-server config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,25 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// Hot reload views via webpack dev server.
+// https://github.com/rails/webpacker/issues/1879
+// However, Circle CI system test choke on this. NODE_ENV is
+// 'development' in tests by default. So check RAILS_ENV and
+// skip hot reload in testing.
+if (process.env.RAILS_ENV == "development") {
+  const chokidar = require("chokidar");
+
+  environment.config.devServer.before = (app, server) => {
+    chokidar
+      .watch([
+        "config/locales/*.yml",
+        "app/assets/stylesheets/**/*.scss",
+        "app/views/**/*.md",
+        "app/views/**/*.html.erb",
+        "app/views/**/*.html.haml",
+      ])
+      .on("change", () => server.sockWrite(server.sockets, "content-changed"));
+  };
+}
+
 module.exports = environment.toWebpackConfig()
